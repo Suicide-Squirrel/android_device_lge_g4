cec19e458d481ffafc1d6bd49107d909801e8ae0
kessaras <theodoroupaulos@gmail.com>
2017-10-16 15:49:36 +0300

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,4 +1,6 @@
 export ANDROID_SET_JAVA_HOME=true
 export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
+export USE_NINJA=true
 
-add_lunch_combo cm_h815-userdebug
+add_lunch_combo aoscp_h815-userdebug
+add_lunch_combo aoscp_h815-eng
