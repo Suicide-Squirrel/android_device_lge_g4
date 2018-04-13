cfe7f2e15aaac84384e1bb99f0fe90389aad6813
ThePiGuy <thepiguy0@gmail.com>
2018-04-13 14:11:11 +0100

    Rebrand to lineage from aoscp Rebrand to lineage and fix any paths that changed in the conversion

---
+++ lineage.mk
@@ -0,0 +1,18 @@
+# Inherit Lineage common bits
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+# Inherit AOSP device configuration for H815
+$(call inherit-product, device/lge/h815/full_h815.mk)
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 2560
+TARGET_SCREEN_WIDTH := 1440
+
+# Setup device specific product configuration
+PRODUCT_NAME := lineage_h815
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_DEVICE="g4" \
+    PRODUCT_NAME="g4_global_com" \
+    BUILD_FINGERPRINT="lge/p1_global_com/p1:6.0/MRA58K/152940055675e:user/release-keys" \
+    PRIVATE_BUILD_DESC="p1_global_com-user 6.0 MRA58K 152940055675e release-keys"
