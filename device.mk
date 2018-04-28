918743800bb57d8b8c78ad822d9d7f87b29f6980
ThePiGuy0 <thepiguy0@gmail.com>
2018-04-28 19:14:31 +0100

    Fix typo in Audio fix

--- device.mk
+++ device.mk
@@ -29,7 +29,7 @@ PRODUCT_PACKAGES += \
 
 # Audio
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/audio/audio_platform_info.xml:system/etc/audio_platform_info.xml
+    $(LOCAL_PATH)/audio/audio_platform_info.xml:system/etc/audio_platform_info.xml \
     $(LOCAL_PATH)/audio/audio_platform_info.xml:system/vendor/etc/audio_platform_info.xml
 
 # Gps
