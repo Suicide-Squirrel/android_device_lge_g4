3b9747af5d6ea69fa48ece1e794451fd1d9061da
steadfasterX <steadfastX@boun.cr>
2018-05-08 16:38:49 +0200

    audio: removed. definition should be in g4-common
    Change-Id: Iee92c54987ac5564801e952d494e7a8b9178aff3

--- device.mk
+++ device.mk
@@ -25,12 +25,7 @@ DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 # Init configuration
 PRODUCT_PACKAGES += \
     fstab.h815 \
-    init.device.rc \
-
-# Audio
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/audio/audio_platform_info.xml:system/etc/audio_platform_info.xml \
-    $(LOCAL_PATH)/audio/audio_platform_info.xml:system/vendor/etc/audio_platform_info.xml
+    init.device.rc 
 
 # Gps
 PRODUCT_COPY_FILES += \
