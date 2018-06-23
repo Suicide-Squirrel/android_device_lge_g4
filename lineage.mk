f533a230018027e52b008aa09f84c4c0513774b4
ThePiGuy0 <thepiguy0@gmail.com>
2018-06-23 10:21:50 +0100

    Build fingerprint: Rewrite in correct format
    To prevent vendor image errors

--- lineage.mk
+++ lineage.mk
@@ -16,4 +16,4 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME="g4_global_com" \
     PRIVATE_BUILD_DESC="p1_global_com-user 8.0 NRD90U 171931902b165 release-keys"
 
-BUILD_FINGERPRINT="lge/p1_global_com/p1:8.0/NRD90U/171931902b165:user/release-keys"
+BUILD_FINGERPRINT := "lge/p1_global_com/p1:8.0/NRD90U/171931902b165:user/release-keys"
