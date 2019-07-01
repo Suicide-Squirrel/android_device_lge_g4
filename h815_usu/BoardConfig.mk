d41dd1334c07e6df9048d338401d8172ba183daf
steadfasterX <steadfasterX@gmail.com>
2019-07-01 14:38:53 +0200

    h815,f500: fixing build issues
    fixed: some mk file names are hardcoded, includes not working properly etc

--- h815_usu/BoardConfig.mk
+++ h815_usu/BoardConfig.mk
@@ -1,3 +1,6 @@
+# inherit board config for all g4's
+include device/lge/g4/BoardConfig.mk
+
 # Partitions
 BOARD_SYSTEMIMAGE_PARTITION_SIZE := 4341104640
 BOARD_USERDATAIMAGE_PARTITION_SIZE := 24897388544
