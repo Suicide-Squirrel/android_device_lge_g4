5973569a690e3d1aeabe1597fe050dbce849630a
steadfasterX <steadfasterX@gmail.com>
2019-07-09 11:50:18 +0200

    g4: changing product name to global
    Google Play store checks ro.product.name + ro.product.manufacturer
    to identify if a device is "compatible" for an app or not.
    p1_global_com is the official value for the h815 international/global
    model which is assumed to be the best one for all.

--- lineage_h810_usu.mk
+++ lineage_h810_usu.mk
@@ -31,7 +31,7 @@ CLEAN_MODEL := $(PRODUCT_DEVICE:_usu=)
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_DEVICE="g4" \
-    PRODUCT_NAME="g4_global_com" \
+    PRODUCT_NAME="p1_global_com" \
     PRIVATE_BUILD_DESC="p1_att_us-user 6.0 MRA58K 1617209012672 release-keys"
 
 BUILD_FINGERPRINT := "lge/p1_att_us/p1:6.0/MRA58K/1617209012672:user/release-keys"
