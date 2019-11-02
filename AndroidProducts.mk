1d7fd32d5959ac8b7513aff18d38aaf87decb0b0
steadfasterX <steadfasterX@gmail.com>
2019-11-02 12:09:42 +0100

    g4: switch to COMMON_LUNCH_CHOICES

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,4 +15,42 @@
 #
 
 PRODUCT_MAKEFILES += \
-    $(LOCAL_DIR)/$(TARGET_PRODUCT).mk
+	$(LOCAL_DIR)/lineage_f500_usu.mk \
+	$(LOCAL_DIR)/lineage_h810_usu.mk \
+	$(LOCAL_DIR)/lineage_h811.mk \
+	$(LOCAL_DIR)/lineage_h812_usu.mk \
+	$(LOCAL_DIR)/lineage_h815.mk \
+	$(LOCAL_DIR)/lineage_h815_usu.mk \
+	$(LOCAL_DIR)/lineage_ls991_usu.mk \
+	$(LOCAL_DIR)/lineage_us991_usu.mk \
+	$(LOCAL_DIR)/lineage_vs986_usu.mk
+
+COMMON_LUNCH_CHOICES := \
+	lineage_f500_usu-userdebug \
+	lineage_f500_usu-user \
+	lineage_f500_usu-eng \
+	lineage_h810_usu-userdebug \
+	lineage_h810_usu-user \
+	lineage_h810_usu-eng \
+	lineage_h811-userdebug \
+	lineage_h811-user \
+	lineage_h811-eng \
+	lineage_h812_usu-userdebug \
+	lineage_h812_usu-user \
+	lineage_h812_usu-eng \
+	lineage_h815-userdebug \
+	lineage_h815-user \
+	lineage_h815-eng \
+	lineage_h815_usu-userdebug \
+	lineage_h815_usu-user \
+	lineage_h815_usu-eng \
+	lineage_ls991_usu-userdebug \
+	lineage_ls991_usu-user \
+	lineage_ls991_usu-eng \
+	lineage_us991_usu-userdebug \
+	lineage_us991_usu-user \
+	lineage_us991_usu-eng \
+	lineage_vs986_usu-userdebug \
+	lineage_vs986_usu-user \
+	lineage_vs986_usu-eng
+
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -2,38 +2,3 @@ export ALLOW_MISSING_DEPENDENCIES=true
 export USE_NINJA=true
 export LLVM_ENABLE_THREADS=1
 
-add_lunch_combo lineage_f500_usu-userdebug
-add_lunch_combo lineage_f500_usu-user
-add_lunch_combo lineage_f500_usu_eng
-
-add_lunch_combo lineage_h810_usu-userdebug
-add_lunch_combo lineage_h810_usu-user
-add_lunch_combo lineage_h810_usu_eng
-
-add_lunch_combo lineage_h811_usu-userdebug
-add_lunch_combo lineage_h811_usu-user
-add_lunch_combo lineage_h811_usu_eng
-
-add_lunch_combo lineage_h812_usu-userdebug
-add_lunch_combo lineage_h812_usu-user
-add_lunch_combo lineage_h812_usu_eng
-
-add_lunch_combo lineage_h815-userdebug
-add_lunch_combo lineage_h815-user
-add_lunch_combo lineage_h815_eng
-
-add_lunch_combo lineage_h815_usu-userdebug
-add_lunch_combo lineage_h815_usu-user
-add_lunch_combo lineage_h815_usu_eng
-
-add_lunch_combo lineage_ls991_usu-userdebug
-add_lunch_combo lineage_ls991_usu-user
-add_lunch_combo lineage_ls991_usu_eng
-
-add_lunch_combo lineage_us991_usu-userdebug
-add_lunch_combo lineage_us991_usu-user
-add_lunch_combo lineage_us991_usu_eng
-
-add_lunch_combo lineage_vs986_usu-userdebug
-add_lunch_combo lineage_vs986_usu-user
-add_lunch_combo lineage_vs986_usu_eng
